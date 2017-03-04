cbc829a48d75ade697fd5aee8a5db33c22f7b28e
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2017-03-04 17:42:12 +1100

    Format boot partition properly with UEFI
    Also correctly create the boot partition when booting with UEFI.

--- http/partition.sh
+++ http/partition.sh
@@ -8,9 +8,9 @@ root_partition="${DEVICE}2"
 
 parted "${DEVICE}" --script mklabel gpt
 if [ -d /sys/firmware/efi ]; then
-  parted "${DEVICE}" --script --align=optimal mkpart primary ext2 1MiB 1GiB
-else
   parted "${DEVICE}" --script --align=optimal mkpart ESP fat32 1MiB 1GiB
+else
+  parted "${DEVICE}" --script --align=optimal mkpart primary ext2 1MiB 1GiB
 fi
 parted "${DEVICE}" --script set 1 boot on
 parted "${DEVICE}" --script --align=optimal mkpart primary btrfs 1GiB 100%
@@ -22,9 +22,11 @@ printf 'password' | cryptsetup luksFormat "${root_partition}"
 # cryptroot is the label by which we reference the encrypted partition.
 printf 'password' | cryptsetup open "${root_partition}" cryptroot
 
-# If UEFI we'd use
-# mkfs.fat -F 32 -n 'boot' "${boot_partition}"
-mkfs.ext2 -L 'boot' "${boot_partition}"
+if [ -d /sys/firmware/efi ]; then
+  mkfs.fat -F 32 -n 'boot' "${boot_partition}"
+else
+  mkfs.ext2 -L 'boot' "${boot_partition}"
+fi
 mkfs.btrfs -L 'root' /dev/mapper/cryptroot
 
 # TODO: mount -o defaults,noatime,nodev,nosuid,(noexec)
