41957e33e50ef654db34b4eded1ac376da716f33
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2018-04-22 12:06:11 +1000

    Add 4GiB swap partition
    It was a mistake to not have a swap partition; using up all available
    ram makes the system freeze.

--- http/partition.sh
+++ http/partition.sh
@@ -4,7 +4,8 @@
 
 DEVICE="${1:-/dev/sda}"
 boot_partition="${DEVICE}1"
-root_partition="${DEVICE}2"
+swap_partition="${DEVICE}2"
+root_partition="${DEVICE}3"
 
 parted "${DEVICE}" --script mklabel gpt
 if [ -d /sys/firmware/efi ]; then
@@ -13,7 +14,8 @@ else
   parted "${DEVICE}" --script --align=optimal mkpart primary ext2 1MiB 1GiB
 fi
 parted "${DEVICE}" --script set 1 boot on
-parted "${DEVICE}" --script --align=optimal mkpart primary btrfs 1GiB 100%
+parted "${DEVICE}" --script --align=optimal mkpart primary linux-swap 1GiB 5GiB
+parted "${DEVICE}" --script --align=optimal mkpart primary btrfs 5GiB 100%
 
 # Note: We must have:
 #  - 'keyboard' and 'encrypt' hooks in mkinitcpio.conf
@@ -22,14 +24,17 @@ printf 'password' | cryptsetup luksFormat "${root_partition}"
 # cryptroot is the label by which we reference the encrypted partition.
 printf 'password' | cryptsetup open "${root_partition}" cryptroot
 
+
 if [ -d /sys/firmware/efi ]; then
   mkfs.fat -F 32 -n 'boot' "${boot_partition}"
 else
   mkfs.ext2 -L 'boot' "${boot_partition}"
 fi
+
+mkswap "${swap_partition}"
+swapon "${swap_partition}"
 mkfs.btrfs -L 'root' /dev/mapper/cryptroot
 
-# TODO: mount -o defaults,noatime,nodev,nosuid,(noexec)
 mount /dev/mapper/cryptroot /mnt
 mkdir /mnt/boot
 mount "${boot_partition}" /mnt/boot
