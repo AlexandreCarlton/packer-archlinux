5a8a7b9bd2495149142bfd592a315e6f493e4bad
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2018-05-03 21:44:15 +1000

    Add (commented) code to use iiNet mirror
    This should save on bandwidth if we're running builds over and over
    again.

--- http/install.sh
+++ http/install.sh
@@ -3,8 +3,8 @@
 # Script used to set up a base environment before provisioning with ansible
 # Also configures for vagrant use (which could be done with ansible but we'll see)
 
-set -e # Stop on first error
-set -x # Show what we're executing
+set -e
+set -x
 
 DEVICE="${1:-/dev/sda}"
 
@@ -13,6 +13,11 @@ timedatectl set-ntp true
 
 # Force synchronisation of package database (so we can install reflector)
 pacman --sync --refresh --refresh
+
+# We set this instead of reflector if we're just making a test build (to save on bandwidth).
+# echo 'Server = http://ftp.iinet.net.au/pub/archlinux/$repo/os/$arch' >  /etc/pacman.d/mirrorlist
+# echo 'Server = http://ftp.iinet.net.au/pub/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
+# echo 'Server = http://ftp.iinet.net.au/pub/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 
 # Generate a list of the fastest mirrors (takes about a minute)
 # Pacstrap copies this across into our new installation.
@@ -23,7 +28,7 @@ reflector --protocol https --number 20 --sort rate --save /etc/pacman.d/mirrorli
 
 pacstrap /mnt base base-devel
 
-# Generate an fstab file with labels (not UUIDs)
+# Generate an fstab file with UUID (this can be accessed via encryption).
 genfstab -t UUID /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash < ./chroot.sh
